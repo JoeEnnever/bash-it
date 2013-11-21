ce15dc052b14db25281da74d7b1362ffd47a693e
Joe Ennever <jennever@foursquare.com>
2013-11-21 14:44:23 +0000

    jennever theme, faster git status checking

---
+++ themes/jennever/jennever.theme.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" |"
+SCM_THEME_PROMPT_SUFFIX="${green}|"
+
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX=" ${green}|"
+GIT_THEME_PROMPT_SUFFIX="${green}|"
+
+RVM_THEME_PROMPT_PREFIX="|"
+RVM_THEME_PROMPT_SUFFIX="|"
+
+function ghost_dude { 
+  scm
+  [[ $SCM == $SCM_GIT ]] && echo "Give COMMITS"
+}
+
+function frowny {
+  echo "\$([[ \$? != 0 ]] && echo ':( ')"
+}
+
+function prompt_command() {
+PS1="\n${purple}Laptop ${reset_color}in ${green}\w\n${bold_cyan}༼つ◕_◕༽つ $(ghost_dude) ${green}$(scm_prompt_info) ${red}$(frowny)${green}\$${reset_color} "
+}
+
+PROMPT_COMMAND=prompt_command;
